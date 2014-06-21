70c3f6f08ad5d6ef9a41c4215cbcf1b31f042bae
Tala Sidawi <talasidawi@gmail.com>
2014-06-21 11:21:09 -0400

    created erb files for summersolstice and fixed top margin

--- holiday_controller.rb
+++ holiday_controller.rb
@@ -5,21 +5,25 @@ class HolidayController
 		date1 = Time.new
 		date2 = Time.parse("2014-06-21")
 		if date1.to_date === date2.to_date
-			template = Tilt::ERBTemplate.new('summersolstice.erb')
+			template = Tilt::ERBTemplate.new('erb/ss.erb')
 			template.render {"YES."}
 		else 
-			template = Tilt::ERBTemplate.new('summersolsticeno.erb')
+			template = Tilt::ERBTemplate.new('erb/ssno.erb')
 			template.render {"NO."}
 		end
 	end
 
 	def danthanhs_birthday
 		date1 = Time.new
-		date2 = Time.parse("2014-08-15")
+		date2 = Time.parse("2014-06-21")
 		if date1.to_date === date2.to_date
-			"HAPPY BIRTHDAYY!"
+			template = Tilt::ERBTemplate.new('erb/db.erb')
+			template.render {"HAPPY BIRTHDAYY!"}
 		else
-			"NAW."
+			date1 = Time.new
+			components = Date.diff(date1, Time.parse("2014-08-15"))
+			template = Tilt::ERBTemplate.new('erb/dbno.erb')
+			template.render {components[:difference]}
 		end
 	end
 
